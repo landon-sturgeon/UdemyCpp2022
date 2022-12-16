86f008d66ec62b0a75cf0793b72fb08ebc19aae1
Landon Sturgeon <sturgeon.landon@gmail.com>
2022-12-16 04:52:24 -0800

    added another sln for more expression practice

---
+++ SumOfNConseqNumbers/SumOfNConseqNumbers/SumOfNConseqNumbers.cpp
@@ -0,0 +1,20 @@
+// SumOfNConseqNumbers.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+int main()
+{
+	// variables to hold values for calculations
+	int n		= 0;
+	int Total	= 0;
+	string UserInput;
+
+	// Collect which natural number (basically whole int) to go to
+	
+
+	// always return an int!!!
+	return 0;
+}
