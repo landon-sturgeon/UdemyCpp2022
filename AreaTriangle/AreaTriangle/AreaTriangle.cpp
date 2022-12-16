fb6f6926a2209b822e409e9962761e0ffe106cd4
Landon Sturgeon <sturgeon.landon@gmail.com>
2022-12-16 03:30:42 -0800

    added sln for AreaTriangle to practice cpp expressions

---
+++ AreaTriangle/AreaTriangle/AreaTriangle.cpp
@@ -0,0 +1,36 @@
+// AreaTriangle.cpp : This file contains the 'main' function.
+// The point of this file is to capture knowledge surrounding variable data
+// types and include statments needed for rudimentary programs.
+// 
+// @author: Landon Sturgeon
+// @created: 221216
+//
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    float Base, Height, Area;
+
+    // collect the triangle's base value
+    printf("What is the base of the triangle?: \n");
+    cin >> Base;
+
+    // collect the triangle's height value
+    printf("What is the height of the triangle?: \n");
+    cin >> Height;
+
+    // calculate the area of the triangle (1/2) * base * height
+    Area = 0.5f * Base * Height;
+
+    printf(
+        "The area of a triangle with a base of %d and a Height of %d = %d ", 
+        Base, Height, Area
+    );
+
+    // always have to return an int since main requires it as return value
+    return 0;
+}
