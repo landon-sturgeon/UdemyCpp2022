61c9b6db38b221023e1fe622d24ddf93c72d6737
Landon Sturgeon <sturgeon.landon@gmail.com>
2022-12-16 05:02:33 -0800

    updated ignore to avoid VS2022 temp file, sln for sum of n numbers

--- SumOfNConseqNumbers/SumOfNConseqNumbers/SumOfNConseqNumbers.cpp
+++ SumOfNConseqNumbers/SumOfNConseqNumbers/SumOfNConseqNumbers.cpp
@@ -5,6 +5,10 @@
 #include <string>
 #include <cstdio>
 
+// helpful so we don't need to keep writing std::<blah blah blah>
+// not so helpful since we're loading in all functions in std though...
+using namespace std;
+
 int main()
 {
 	// variables to hold values for calculations
@@ -13,7 +17,19 @@ int main()
 	string UserInput;
 
 	// Collect which natural number (basically whole int) to go to
-	
+	printf("What number do you wish to add up to: ");
+	getline(cin, UserInput);
+	n = atoi(UserInput.c_str());
+
+	// User fancy algorithm to find sum of natural numbers
+	// will always be an integer since inputs were natural numbers
+	Total = n * (n + 1) / 2;
+
+	// Print to the console what the total is
+	printf(
+		"The total for all the numbers leading up to and including %i = %i", 
+		n, Total
+	);
 
 	// always return an int!!!
 	return 0;
